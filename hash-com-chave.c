f01bbc08a7a65bbae3a1a53fd9341f8b1253ff18
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-06-13 11:56:01 -0300

    Atualizando Projeto e Tabela hash de nomes

--- hash-com-chave.c
+++ hash-com-chave.c
@@ -7,10 +7,13 @@
 #define TAM_MAX_NOME 100
 #define NUM_CHAVES 53
 
-typedef struct {
+typedef struct Node {
     char nome[TAM_MAX_NOME];
-    // Adicione outras informações adicionais, se necessário
-} Registro;
+    struct Node* prox;
+    struct Node* ant;
+} Node;
+
+Node* tabela[NUM_CHAVES];
 
 int hash(const char* nome) {
     int soma = 0;
@@ -22,43 +25,108 @@ int hash(const char* nome) {
     return soma % NUM_CHAVES;
 }
 
-int main() {
-    Registro tabela[NUM_CHAVES];
+void inserirNome(const char* nome) {
+    int chave = hash(nome);
 
-    // Inicializar todas as posições como vazias
-    for (int i = 0; i < NUM_CHAVES; i++) {
-        tabela[i].nome[0] = '\0';
+    // Criar um novo nó para o nome
+    Node* novoNome = (Node*) malloc(sizeof(Node));
+    strcpy(novoNome->nome, nome);
+    novoNome->prox = NULL;
+    novoNome->ant = NULL;
+
+    // Verificar se a chave está vazia
+    if (tabela[chave] == NULL) {
+        tabela[chave] = novoNome;
+    } else {
+        // Caso contrário, inserir no final da lista
+        Node* atual = tabela[chave];
+        while (atual->prox != NULL) {
+            atual = atual->prox;
+        }
+        atual->prox = novoNome;
+        novoNome->ant = atual;
+    }
+}
+
+int consultarNome(const char* nome) {
+    int chave = hash(nome);
+
+    // Verificar se a chave está vazia
+    if (tabela[chave] == NULL) {
+        return 0;
     }
 
-    // Abrir o arquivo
-    FILE *arquivo = fopen("nomes.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return 1;
+    // Procurar o nome na lista
+    Node* atual = tabela[chave];
+    while (atual != NULL) {
+        if (strcmp(atual->nome, nome) == 0) {
+            return 1; // Nome encontrado
+        }
+        atual = atual->prox;
     }
 
-    // Ler os nomes do arquivo e inserir na tabela hash
-    char linha[TAM_MAX_NOME];
-    while (fgets(linha, TAM_MAX_NOME, arquivo) != NULL) {
-        // Remover o caractere de nova linha (\n) da string
-        linha[strcspn(linha, "\n")] = '\0';
+    return 0; // Nome não encontrado
+}
 
-        // Calcular a chave usando a função de hash
-        int chave = hash(linha);
+int contarElementosPorChave(int chave) {
+    int count = 0;
 
-        // Inserir o nome na tabela hash
-        strcpy(tabela[chave].nome, linha);
+    // Verificar se a chave está vazia
+    if (tabela[chave] == NULL) {
+        return 0;
     }
 
-    // Fechar o arquivo
-    fclose(arquivo);
+    // Contar os elementos na lista
+    Node* atual = tabela[chave];
+    while (atual != NULL) {
+        count++;
+        atual = atual->prox;
+    }
 
-    //configurar o gnuplot- para plotar no gráfico
+    return count;
+}
 
-    gnuplot_ctrl *gp;
+void removerNome(const char* nome) {
+    int chave = hash(nome);
+
+    // Verificar se a chave está vazia
+    if (tabela[chave] == NULL) {
+        return;
+    }
+
+    // Procurar o nome na lista
+    Node* atual = tabela[chave];
+    while (atual != NULL) {
+        if (strcmp(atual->nome, nome) == 0) {
+            // Remover o nó da lista
+            if (atual->ant == NULL) {
+                // O nó é o primeiro da lista
+                tabela[chave] = atual->prox;
+            } else {
+                atual->ant->prox = atual->prox;
+            }
+            if (atual->prox != NULL) {
+                atual->prox->ant = atual->ant;
+            }
+            free(atual);
+            return;
+        }
+        atual = atual->prox;
+    }
+}
+
+void exibirHistograma() {
+    // Configurar o gnuplot para plotagem
+    gnuplot_ctrl* gp;
     gp = gnuplot_init();
 
     // Preparar os dados para plotagem
+    int contagens[NUM_CHAVES] = {0};
+    for (int i = 0; i < NUM_CHAVES; i++) {
+        contagens[i] = contarElementosPorChave(i);
+    }
+
+    // Enviar os dados para o gnuplot
     char comando[100];
     sprintf(comando, "set xlabel 'Chave'");
     gnuplot_cmd(gp, comando);
@@ -71,7 +139,6 @@ int main() {
     sprintf(comando, "plot '-' using 1:2 with boxes");
     gnuplot_cmd(gp, comando);
 
-    // Enviar os dados para o gnuplot
     for (int i = 0; i < NUM_CHAVES; i++) {
         sprintf(comando, "%d %d", i, contagens[i]);
         gnuplot_cmd(gp, comando);
@@ -84,16 +151,61 @@ int main() {
 
     // Fechar o gnuplot
     gnuplot_close(gp);
+}
 
+int main() {
+    // Inicializar todas as posições como vazias
+    for (int i = 0; i < NUM_CHAVES; i++) {
+        tabela[i] = NULL;
+    }
 
-    // buscar um nome específico
+    // Abrir o arquivo
+    FILE* arquivo = fopen("nomes.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    // Ler os nomes do arquivo e inserir na tabela hash
+    char linha[TAM_MAX_NOME];
+    while (fgets(linha, TAM_MAX_NOME, arquivo) != NULL) {
+        // Remover o caractere de nova linha (\n) da string
+        linha[strcspn(linha, "\n")] = '\0';
+
+        inserirNome(linha);
+    }
+
+    // Fechar o arquivo
+    fclose(arquivo);
+
+    // Exibir o histograma
+    exibirHistograma();
+
+    // Buscar um nome específico
     char nome_desejado[TAM_MAX_NOME] = "João";
     int chave_desejada = hash(nome_desejado);
 
-    if (strcmp(tabela[chave_desejada].nome, nome_desejado) == 0) {
+    if (consultarNome(nome_desejado)) {
         printf("O nome %s está na base de dados.\n", nome_desejado);
     } else {
         printf("O nome %s não foi encontrado na base de dados.\n", nome_desejado);
+    }
+
+    // Remover um nome da base de dados
+    char nome_remover[TAM_MAX_NOME] = "Maria";
+    removerNome(nome_remover);
+
+    // Exibir o histograma atualizado após a remoção
+    exibirHistograma();
+
+    // Liberar a memória alocada
+    for (int i = 0; i < NUM_CHAVES; i++) {
+        Node* atual = tabela[i];
+        while (atual != NULL) {
+            Node* prox = atual->prox;
+            free(atual);
+            atual = prox;
+        }
     }
 
     return 0;
